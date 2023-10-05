01153241a1e5d774ac22b395d1e177c5d1a78ead
Sandeep Reddy Munagala <129391713+munagalasandeep99@users.noreply.github.com>
2023-10-05 15:02:31 +0200

    Update de.py

--- de.py
+++ de.py
@@ -1,33 +1,24 @@
-"""Support for functionality to download files."""
 from http import HTTPStatus
 import logging
 import os
 import re
 import threading
-
 import requests
 import voluptuous as vol
-
 from homeassistant.core import HomeAssistant, ServiceCall
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.typing import ConfigType
-from homeassistant.util import raise_if_invalid_filename, raise_if_invalid_path
 
 _LOGGER = logging.getLogger(__name__)
-
 ATTR_FILENAME = "filename"
 ATTR_SUBDIR = "subdir"
 ATTR_URL = "url"
 ATTR_OVERWRITE = "overwrite"
-
 CONF_DOWNLOAD_DIR = "download_dir"
-
 DOMAIN = "downloader"
 DOWNLOAD_FAILED_EVENT = "download_failed"
 DOWNLOAD_COMPLETED_EVENT = "download_completed"
-
 SERVICE_DOWNLOAD_FILE = "download_file"
-
 SERVICE_DOWNLOAD_FILE_SCHEMA = vol.Schema(
     {
         vol.Required(ATTR_URL): cv.url,
@@ -36,7 +27,6 @@ SERVICE_DOWNLOAD_FILE_SCHEMA = vol.Schema(
         vol.Optional(ATTR_OVERWRITE, default=False): cv.boolean,
     }
 )
-
 CONFIG_SCHEMA = vol.Schema(
     {DOMAIN: vol.Schema({vol.Required(CONF_DOWNLOAD_DIR): cv.string})},
     extra=vol.ALLOW_EXTRA,
@@ -56,10 +46,25 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     def download_file(service: ServiceCall) -> None:
         """Start thread to download the file specified in the URL."""
         url = service.data[ATTR_URL]
-        subdir = service.data.get(ATTR_SUBDIR)
+        subdir = service.data.get(ATTR_SUBDIR) or ""
         filename = service.data.get(ATTR_FILENAME)
         overwrite = service.data.get(ATTR_OVERWRITE)
         threading.Thread(target=do_download, args=(url, subdir, filename, overwrite)).start()
+
+    def do_download(url, subdir, filename, overwrite):
+        """Download the file."""
+        try:
+            filename = resolve_filename(url, filename)
+            final_path = create_final_path(download_path, subdir, filename, overwrite)
+            _LOGGER.debug("%s -> %s", url, final_path)
+            download_and_save_file(url, final_path)
+            _LOGGER.debug("Downloading of %s done", url)
+            hass.bus.fire(
+                f"{DOMAIN}_{DOWNLOAD_COMPLETED_EVENT}",
+                {"url": url, "filename": filename},
+            )
+        except (requests.exceptions.ConnectionError, ValueError) as e:
+            handle_download_failure(url, filename, final_path, e)
 
     hass.services.register(
         DOMAIN,
@@ -68,28 +73,6 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
         schema=SERVICE_DOWNLOAD_FILE_SCHEMA,
     )
     return True
-
-def do_download(url, subdir, filename, overwrite):
-    """Download the file."""
-    try:
-        subdir = subdir or ""
-        raise_if_invalid_path(subdir)
-
-        filename = resolve_filename(url, filename)
-        raise_if_invalid_filename(filename)
-
-        final_path = create_final_path(download_path, subdir, filename, overwrite)
-
-        _LOGGER.debug("%s -> %s", url, final_path)
-        download_and_save_file(url, final_path)
-
-        _LOGGER.debug("Downloading of %s done", url)
-        hass.bus.fire(
-            f"{DOMAIN}_{DOWNLOAD_COMPLETED_EVENT}",
-            {"url": url, "filename": filename},
-        )
-    except (requests.exceptions.ConnectionError, ValueError) as e:
-        handle_download_failure(url, filename, final_path, e)
 
 def resolve_filename(url, filename):
     """Resolve the filename to use for the downloaded file."""
